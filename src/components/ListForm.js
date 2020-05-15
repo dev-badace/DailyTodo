d04533ae6ce8069c9a41f962a36e666bf70d56b0
shivam <shibru127@gmail.com>
2020-05-15 16:27:45 +0530

    basic changes

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -4,7 +4,7 @@ export default (props) => {
   
   return(
     <form className="list__form" onSubmit={props.onSubmit} autoComplete="off">
-       <input className="list__form--input" type="text" name="search" placeholder="Add A New Todo"/>
+       <input className="list__form--input" type="text" name="search" placeholder="Add A New Todo" spellCheck="false"/>
        <button className="list__form--button-add"> &#10003; </button>
     </form>
   )
