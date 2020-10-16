f9d2f89d04cd4d2c8aed8580fc1b6749433f1929
shivam <shibru127@gmail.com>
2020-10-16 19:39:04 +0530

    mega sphageeti code

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,129 @@
 /*eslint-disable no-unused-vars */
 
-import React,{useState,useEffect} from 'react'
-import moment from 'moment'
-import ListForm from './ListForm'
-import ListItem from './ListItem'
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import ListForm from "./ListForm";
+import ListItem from "./ListItem";
 //TODO create app component create singleItem component wire them and done
 
 const App = () => {
+  const [list, setList] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
- const [list,setList] = useState([])
+  useEffect(() => {
+    try {
+      const listData = localStorage.getItem("todoList");
+      const data = JSON.parse(listData);
 
- useEffect(() => {
-   try{
-    const listData = localStorage.getItem('todoList')
-    const data = JSON.parse(listData)
-    
-    if(data) {
-      setList(data)
-    }
-   }catch(e) {}
- },[])
+      if (data) {
+        if (data.length > 0 && typeof data[0] === "string") {
+          const revised = data.map((str) => {
+            return { title: str, list: [] };
+          });
+          const json = JSON.stringify(revised);
+          localStorage.setItem("todoList", json);
+        }
+      }
+    } catch (e) {}
+  }, []);
 
- useEffect(() => {
-   const json = JSON.stringify(list)
-   localStorage.setItem('todoList',json)
- },[list])
- 
- const onFormSubmit = (e) => {
-   e.preventDefault()
-   setList(list.concat(e.target.search.value.trim()))
-   e.target.search.value = ''
- }
- 
- const remove = (todo) => {
-   const newList = list.filter((i) => i!== todo)
-   setList(newList)
- }
+  useEffect(() => {
+    try {
+      const listData = localStorage.getItem("todoList");
+      const data = JSON.parse(listData);
 
- const removeAll = () => {
-   setList([])
- }
- return (
-   <div className="main">
-      <h2 className="remove-all" onClick={removeAll}>&#x0233D;</h2>
-      <h3 className="current-day">{moment().format('l')}</h3>
-      <ListForm className="list-form" onSubmit={onFormSubmit} />
+      if (data) {
+        setList(data);
+      }
+    } catch (e) {}
+  }, []);
+
+  useEffect(() => {
+    const json = JSON.stringify(list);
+    localStorage.setItem("todoList", json);
+  }, [list]);
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    if (!e.target.search.value) return;
+    const obj = {};
+    obj.title = e.target.search.value.trim();
+    obj.list = [];
+    setList(list.concat(obj));
+    e.target.search.value = "";
+  };
+
+  const edit = (todo, newName) => {
+    const newList = list.map(({ title, list }) => {
+      if (title === todo) {
+        return { title: newName, list };
+      }
+      return { title, list };
+    });
+
+    console.log("neew", newList);
+    setList(newList);
+  };
+
+  const remove = (todo) => {
+    const newList = list.filter(({ title }) => title !== todo);
+    setList(newList);
+  };
+
+  const listAdd = (todo, listAdd) => {
+    const newData = list.map(({ title, list }) => {
+      if (title === todo) {
+        return { title, list: [...list, listAdd] };
+      }
+
+      return { title, list };
+    });
+
+    setList(newData);
+  };
+
+  const listRemove = (todo, listRemove) => {
+    const newData = list.map(({ title, list }) => {
+      if (title === todo) {
+        const newList = list.filter((tlist) => tlist !== listRemove);
+        return { title, list: newList };
+      }
+
+      return { title, list };
+    });
+
+    setList(newData);
+  };
+
+  return (
+    <div className="main">
+      <h3 className="current-day">{moment().format("l")}</h3>
+      <h3
+        className="remove-all"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        +
+      </h3>
+      {isOpen && <ListForm className="list-form" onSubmit={onFormSubmit} />}
+
       {list.length === 0 && <p className="red-alert">Please add a Todo...</p>}
-      {list.map((soloItem) => (<ListItem className="list-item" key={soloItem} todo={soloItem} remove={remove} />))}
-   </div>
- )
+      {console.log(list)}
+      {list.map((soloItem) => (
+        <ListItem
+          className="list-item"
+          key={soloItem.title}
+          todo={soloItem.title}
+          list={soloItem.list}
+          listAdd={listAdd}
+          listRemove={listRemove}
+          remove={remove}
+          edit={edit}
+        />
+      ))}
+    </div>
+  );
+};
 
-}
-
-export default App
+export default App;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
+import ListForm from "./ListForm";
 
 export default (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const [inputTodo, setInputTodo] = useState(props.todo || "");
+  const [newTodo, setNewTodo] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    props.edit(props.todo, e.target.hash.value.trim());
+    setClicked(!clicked);
+  };
+
+  const listSubmit = (e) => {
+    e.preventDefault();
+    if (!e.target.search.value) return;
+
+    props.listAdd(props.todo, e.target.search.value.trim());
+    e.target.search.value = "";
+  };
+  console.log(newTodo);
   return (
-    <div className="list__item">
-      <h3 className="list__item--heading">{props.todo}</h3>
-      <button className="list__item--button-remove" onClick={() => {
-        props.remove(props.todo)
-      }}>&#x02717;</button>
+    <div className="list__head" onDoubleClick={() => setClicked(!clicked)}>
+      {clicked ? (
+        <>
+          <form className="list__item" onSubmit={onSubmit}>
+            <input
+              type="text"
+              name="hash"
+              value={inputTodo}
+              onChange={(e) => setInputTodo(e.target.value)}
+            />
+          </form>
+        </>
+      ) : (
+        <>
+          <div className="list__item">
+            <h3
+              className="list__item--heading"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {props.todo}
+            </h3>
+            <>
+              <h2 onClick={() => setNewTodo(!newTodo)}>+</h2>
+              <button
+                className="list__item--button-remove"
+                onClick={() => {
+                  props.remove(props.todo);
+                }}
+              >
+                &#x02717;
+              </button>
+            </>
+          </div>
+        </>
+      )}
+
+      {isOpen && (
+        <>
+          {newTodo && <ListForm onSubmit={listSubmit} />}
+          {props.list.length === 0 && (
+            <div className="red-alert">No Data in {props.todo}</div>
+          )}
+          {props.list &&
+            props.list.map((data) => {
+              return (
+                <div key={data} className="list__item-child">
+                  <h3 className="list__item-child--heading">{data}</h3>
+                  <button
+                    className="list__item-child--button-remove"
+                    onClick={() => {
+                      props.listRemove(props.todo, data);
+                    }}
+                  >
+                    &#x02717;
+                  </button>
+                </div>
+              );
+            })}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
